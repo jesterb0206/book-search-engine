8138ea650f2996efed206294b7c95f1c62b4132b
jesterb0206 <jesterb@seattleu.edu>
2023-01-14 02:50:01 -0800

    Update the Auth Middleware Function to Work With the GraphQL API

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,39 +1,40 @@
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
-// set token secret and expiration date
-const secret = 'mysecretsshhhhh';
+const secret = process.env.JWT_SECRET;
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+  authMiddleware: function ({ req }) {
+    // Allows the JSON Web Token to Be Sent via req.body, req.query, or req.headers
 
-    // ["Bearer", "<tokenvalue>"]
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    // Splits the Token String Into an Array and Returns The Actual Token
+
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
 
-    // verify token and get user data out of it
+    // If the Token Can Be Verified, Add the Decoded User’s Data to the Request So It Can Be Accessed in the Resolver
+
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
     }
 
-    // send to next endpoint
-    next();
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
+    // Returns the Request Object So It Can Be Passed to the Resolver as `Context`
 
+    return req;
+  },
+  signToken: function ({ email, username, _id }) {
+    const payload = { email, username, _id };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
