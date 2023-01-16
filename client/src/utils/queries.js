86e66c28908de24b8f48b6a3056708ae28d640ce
jesterb0206 <jesterb@seattleu.edu>
2023-01-15 22:32:01 -0800

    Made It So queries.js Will Execute the Me Query Set Up Using Apollo Server

---
+++ client/src/utils/queries.js
@@ -0,0 +1,20 @@
+import gql from 'graphql-tag';
+
+export const GET_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
